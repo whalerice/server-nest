854f1ddd59ef29e1ec8e3bc6a2e2b7d1b990a876
whalerice <54788767+whalerice@users.noreply.github.com>
2024-03-14 23:36:48 +0900

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: 'https://server-nest-khaki.vercel.app',
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+    // credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
     // exposedHeaders: ['Authorization'], // * 사용할 헤더 추가.
   });
   await app.listen(3300);
