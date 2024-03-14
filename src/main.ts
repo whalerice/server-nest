87e22ea21de44b61f271d8d4d67c92df0ef963db
taerim.woo <taerim.woo@quantum-ai.ai>
2024-03-14 18:10:24 +0900

    modify

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: true,
+    credentials: true,
+    // exposedHeaders: ['Authorization'], // * 사용할 헤더 추가.
+  });
   await app.listen(3300);
 }
 bootstrap();
