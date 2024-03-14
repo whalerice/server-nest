08099c67b24f905fa7b235303bc71d741933dee0
taerim.woo <taerim.woo@quantum-ai.ai>
2024-03-14 17:14:06 +0900

    배포용 내용 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { UsersModule } from './users/users.module';
-import { EnterprisesModule } from './enterprises/enterprises.module';
 
 @Module({
-  imports: [UsersModule, EnterprisesModule],
+  imports: [UsersModule],
   controllers: [AppController],
   providers: [],
 })
