f4834d7d94df32f507c8f189108a688a7b9fdc94
whalerice <54788767+whalerice@users.noreply.github.com>
2024-03-14 23:39:51 +0900

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://server-nest-khaki.vercel.app',
+    origin: 'https://sooni-r.vercel.app',
     // credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE']
     // exposedHeaders: ['Authorization'], // * 사용할 헤더 추가.
