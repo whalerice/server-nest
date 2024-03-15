971d1b6eb58c794a7a2086bcb536258a436e065d
taerim.woo <taerim.woo@quantum-ai.ai>
2024-03-15 10:15:30 +0900

    dte

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,19 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: 'https://sooni-r.vercel.app',
-    // credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE']
-    // exposedHeaders: ['Authorization'], // * 사용할 헤더 추가.
+  // app.enableCors({
+  //   origin: 'https://sooni-r.vercel.app',
+  //   // credentials: true,
+  //   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  //   // exposedHeaders: ['Authorization'], // * 사용할 헤더 추가.
+  // });
+  app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', req.header('Origin'));
+    res.header(
+      'Access-Control-Allow-Methods',
+      'GET, POST, PUT, DELETE, OPTIONS',
+    );
+    next();
   });
   await app.listen(3300);
 }
