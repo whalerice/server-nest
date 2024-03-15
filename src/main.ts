626870a1752822f478896bb8c3043095b330976f
taerim.woo <taerim.woo@quantum-ai.ai>
2024-03-15 11:34:34 +0900

    정리

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['https://sooni-r.vercel.app'],
+    origin: ['https://sooni-r.vercel.app', 'http://localhost:3200'],
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    exposedHeaders: [''],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    exposedHeaders: ['Content-Type'],
   });
 
   await app.listen(3300);
